0d19ab44d9b62ba049618caf66efa6821a133309
murathe <isaacsm07p@gmail.com>
2020-01-11 12:45:15 +0300

    Define major routes

--- main.py
+++ main.py
@@ -4,3 +4,20 @@ main = Flask(__name__)
 @main.route('/')
 @main.route('/index')
 def Index:
+    return render_template('index.html')
+
+@main.route('/interview')
+def Interview:
+    return render_template('interview.html')
+
+@main.route('/leadership.html')
+def Leadership:
+    return render_template('leadership.html')
+
+@main.route('/product')
+def Product:
+    return render_template('product.html')
+
+
+if __name__ == "__main__":
+    main.run(debug=True)
