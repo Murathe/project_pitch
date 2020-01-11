3a09e3fece1e42fb62bf0f52fab3e816cb6718ae
murathe <isaacsm07p@gmail.com>
2020-01-11 13:50:23 +0300

    Email text field

--- forms.py
+++ forms.py
@@ -3,4 +3,5 @@ from wtforms import StringField
 from wtforms.validators import DataRequired, Length
 
 class Signup_form(wtforms):
-    username = StringField('username', validators=[])
+    username = StringField('Username', validators=[DataRequired(), Length(min=5, max=15)])
+    email =StringField('Email', validators=[DataRequired(), Email()])
