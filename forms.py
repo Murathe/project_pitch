0d6d13a03f262e9512703556670a196c5303ab7b
murathe <isaacsm07p@gmail.com>
2020-01-11 13:48:59 +0300

    Username text field

--- forms.py
+++ forms.py
@@ -1,1 +1,6 @@
 from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired, Length
+
+class Signup_form(wtforms):
+    username = StringField('username', validators=[])
