b5fd027c9cd6a3e143c2184166a903a032a754c6
murathe <isaacsm07p@gmail.com>
2020-01-11 13:55:25 +0300

    Password & subbmit field

--- forms.py
+++ forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class Signup_form(wtforms):
     username = StringField('Username', validators=[DataRequired(), Length(min=5, max=15)])
-    email =StringField('Email', validators=[DataRequired(), Email()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Passowrd', validators=[DataRequired(),])
+    confirm_password = PasswordField('Confirm password', validators=[DataRequired(), EqualTo('password')])
