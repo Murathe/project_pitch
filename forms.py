9b8ff089abb826dd133cd590544d3742610c2284
murathe <isaacsm07p@gmail.com>
2020-01-12 17:25:47 +0300

    Corect the file structure

--- forms.py
+++ forms.py
@@ -1,9 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
-class Signup_form(wtforms):
+class SignupForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=5, max=15)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Passowrd', validators=[DataRequired(),])
     confirm_password = PasswordField('Confirm password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+class SigninForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Passowrd', validators=[DataRequired(),])
+    remember_me = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
--- main.py
+++ main.py
@@ -1,46 +1,49 @@
-from flask import Flask,render_template, url_for
-from forms import Signup_form, Signin_form
+from flask import Flask,render_template, url_for, flash
+from forms import SignupForm, SigninForm
 main = Flask(__name__)
 
 main.config['SECRET_KEY'] = '35c918c62246'
 
-eg = [
-    {'one' : 'baby'}
-    {'two': 'mother'}
-    {'three': 'father'}
-]
+egs = [
+    {'one' : 'baby',
+    'two': 'mother',
+    'three': 'father'},
 
+    {'one' : 'babies',
+    'two': 'mothers',
+    'three': 'fathers'}
+]
 
 @main.route('/')
 @main.route('/index')
-def Index:
-    return render_template('index.html', eg=eg)
+def index():
+    return render_template('index.html', title='Project Pitch', egs=egs)
 
 @main.route('/signup')
-def Signup:
+def Signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        flash(f'Account created succesfully for {form.username.data}.')
 
-    form = Signup_form()
-
-    return render_template('sign_up', title='Sign Up', form=form)
+    return render_template('sign_up.html', title='Sign Up', form=form)
 
 @main.route('/signin')
-def Signup:
+def Signin():
+    form = SigninForm()
 
-    form = Signup_form()
-
-    return render_template('sign_up', title='Sign Up', form=form)
+    return render_template('sign_in.html', title='Sign In', form=form)
 
 @main.route('/interview')
-def Interview:
-    return render_template('interview.html')
+def Interview():
+    return render_template('interview.html', title='Sign In')
 
 @main.route('/leadership.html')
-def Leadership:
-    return render_template('leadership.html')
+def Leadership():
+    return render_template('leadership.html', title='Sign In')
 
 @main.route('/product')
-def Product:
-    return render_template('product.html')
+def Product():
+    return render_template('product.html', title='Product')
 
 
 if __name__ == "__main__":
